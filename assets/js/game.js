112a8ef73888717f4999af2275cd59b1807693ff
rcintron1 <rolando.cintron@gmail.com>
2017-08-19 01:55:49 -0400

    Fixing bug on last letter

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,17 +4,24 @@
 // select a random word
 var varWords = ["union","research","cruelty","shadow","crown","abduction","applause","undersea","glutton","eternity","endorsement"];
 var randomWord;
+var usedLetters;
 // Keep a record of all selected letters
 var guessedLetters;
 // Location in page to place all selected letters
 var spanguessedL = document.getElementById("guessedL");
 // Location of underscores that will be replaced with correct letters
 var spanWord = document.getElementById("spanWord");
-
+// Used to populate Wins span
+var wins = document.getElementById("wins");
+// Used to populate Losses span
+var losses = document.getElementById("losses");
+// Used to find out if all characters have been matched
+var randomWordcharCount = 0;
+var wrongLetters = 8;
 
 // Eventhandler for key pressed
 document.onkeyup = function(event){
-  console.log(event.key);
+  console.log("onkeyup:" + event.key);
   compareLetters(event.key);
 }
 
@@ -23,19 +30,50 @@ function startGame(){
   //Math.Random() returns a random float between 0 and 1 (including 0, excluding 1)
   randomWord = varWords[Math.floor(Math.random() * varWords.length)];
   guessedLetters = "";
+  usedLetters = "";
   spanguessedL.textContent = "";
   replaceUnder();
+  wrongLetters = 8;
 }
 
 // Function to see if letter is in random word
 // Also used to update the counts
 function compareLetters(letter){
+  if (usedLetter(letter)){
+    console.log("function compareLetters -> letter" + " has been used")
+    alert("Letter has already been used!")
+    return;
+  }
   console.log("function->compareLetters")
   if(randomWord.includes(letter)){
     replaceUnder(letter);
   }else{
     guessedLetters += letter;
     spanguessedL.textContent = guessedLetters;
+    wrongLetters--;
+  }
+  usedLetters += letter;
+  setTimeout(checkGame(), 1000);
+}
+
+function usedLetter(letter){
+  if (usedLetters.includes(letter)){
+    console.log("")
+    return true;
+  }else{
+    return false;
+  }
+}
+
+function checkGame(){
+  if (randomWordcharCount == 0){
+    wins.textContent = Number(wins.textContent) + 1;
+    alert("You Won");
+    startGame();
+  }else if (wrongLetters == 0) {
+    losses.textContent = Number(losses.textContent) + 1;
+    alert("You Lose");
+    startGame();
   }
 }
 
@@ -50,12 +88,15 @@ function replaceUnder(letter){
         x = spanWord.textContent;
         x = x.substring(0,i) + letter + x.substring(i+1);
         spanWord.textContent = x;
-        console.log(x);
+        randomWordcharCount--;
+        console.log("function->replaceUnder:" +x + " Letters left=" + randomWordcharCount.toString());
       }
     }
   }else{
     // create underscores on page, should only happen in beginning of game
     spanWord.textContent = Array(randomWord.length + 1).join("_");
+    randomWordcharCount = randomWord.length
   }
 }
+
 startGame();
